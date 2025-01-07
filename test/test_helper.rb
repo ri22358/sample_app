3cd8c1c89af59277308e059e5811adaf746e1da4
Rion Yokoyama <j122358@g.kogakuin.jp>
2025-01-07 07:24:58 +0000

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,11 @@
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+  # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # test/fixtures/*.ymlのfixtureをすべてセットアップする
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
